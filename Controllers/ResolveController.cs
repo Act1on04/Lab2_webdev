ca8899b65c60177d6db7bb3ab8aa17ce156ddeee
Alex Ishchenko <101579409+Act1on04@users.noreply.github.com>
2023-10-28 21:27:48 +0300

    Add project files.

---
+++ Controllers/ResolveController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lab2_vs.Controllers
+{
+    public class ResolveController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
